daf8e113e53282cb7cf5c08d4778e0e17f195ce3
FJMarfil <Franciski_98@hotmail.com>
2023-03-09 01:04:33 +0100

    Add click listeners to navbar icon and close button

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
-hello = 1;
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    console.log("Se ha clickeado", bar);
+    nav.classList.add("active");
+  });
+}
+
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
