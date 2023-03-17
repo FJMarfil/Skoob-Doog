55ddfb14a0a7785e6c080dd2c54d63ce72f3bb48
FJMarfil <Franciski_98@hotmail.com>
2023-03-17 02:30:02 +0100

    Add connection to db file

---
+++ php/connect-to-database.php
@@ -0,0 +1,22 @@
+<?php
+
+//Nombre del servidor
+$serverName = "localhost";
+//Nombre de usuario
+$userName  = "franmarfil";
+//Contraseña
+$password = "Mediterraneo@23";
+//Nombre de base de datos
+$dataBase = "skoobdoog";
+
+//Conexión con base de datos
+$conn = new mysqli($serverName, $userName, $password, $dataBase);
+
+//Si la conexión falla nos mostrará un mensaje de error
+if ($conn->connect_error) {
+    die("La conexión ha fallado: " . $conn->connect_error);
+} else {
+    echo "Conexión realizada";
+}
+
+?>
