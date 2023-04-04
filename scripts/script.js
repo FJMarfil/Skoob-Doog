58bf58493281746f0924a9594507f83b7a90ab6d
FJMarfil <Franciski_98@hotmail.com>
2023-04-05 01:06:44 +0200

    Add TODO comment

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
 
-//todo: Quitar comentario de consola
+// TODO:  Quitar comentario de consola
 if (bar) {
   bar.addEventListener("click", () => {
     console.log("Se ha clickeado", bar);
