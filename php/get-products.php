4e144c5cce22e898ba223d70c85150658f61682f
FJMarfil <Franciski_98@hotmail.com>
2023-04-05 20:21:09 +0200

    Add: function to get products from db

---
+++ php/get-products.php
@@ -0,0 +1,43 @@
+<?php
+
+function getProducts()
+{
+    // Conexión a base de datos
+    require("connect-to-database.php");
+
+    // Consulta para obtener datos de la base de datos
+    $query = "SELECT * FROM book";
+
+    // Ejecutar la consulta
+    $result = mysqli_query($conn, $query);
+
+    // Comprobar si hay algún error en la consulta
+    if (!$result) {
+        echo "Error en la consulta: " . mysqli_errno($conn);
+        exit();
+    }
+
+    // Crear array para almacenar los datos de la base de datos
+    $products = array();
+
+    // Obtener los datos de la base de datos y agregarlos al array
+    while ($row = mysqli_fetch_assoc($result)) {
+        $product = array(
+            "isbn" => $row["book_isbn"],
+            "title" => $row["book_title"],
+            "price" => $row["book_price"],
+            "stock" => $row["book_stock"],
+            "author" => $row["author_id"],
+            "publisher" => $row["publisher_id"]
+        );
+        array_push($products, $product);
+    }
+
+    // Cerrar la conexión con la base de datos
+    mysqli_close($conn);
+
+    // Devolver array con los datos
+    return $products;
+}
+
+?>
