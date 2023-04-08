f30c2d31a046a832eb69778d77065694a460255e
FJMarfil <Franciski_98@hotmail.com>
2023-04-08 03:10:27 +0200

    Add: function that transform db array data to JSON

---
+++ php/get-products-json.php
@@ -0,0 +1,17 @@
+<?php
+
+// Incluir datos del archivo get-products.php
+require("get-products.php");
+
+// Covertir los datos a un formato JSON, para poder utilizarlo en los archivos JavaScript
+
+// Obtener datos de la base de datos y almacenarlos en la variable $products
+$products = getProducts();
+
+// Convertir el array a una cadena JSON
+$productsJson = json_encode($products);
+
+// Imprimir la cadena JSON
+echo $productsJson;
+
+?>
