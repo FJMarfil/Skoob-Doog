6b8e40425498f56e4d53a327ef45a833cd5184f1
FJMarfil <Franciski_98@hotmail.com>
2023-04-17 01:11:17 +0200

    Add: show product data when click on cart icon

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,34 +1,44 @@
-// Variable que del contenedor donde estarán
-const cartContainerTable = document.querySelector(".pro-container");
+// Declaración de constantes y variables
+const cartContainerTable = document.querySelector(".pro-container"); // Contenedor donde estarán los productos
+
+let cart = []; // Declarar variable de carrito
+let dataArray = []; // Declarar la variable con el array de datos
 
 // Obtener datos de productos en formato JSON
-fetch("php/get-products-json.php")
+const fetchPromise = fetch("php/get-products-json.php")
   .then((res) => res.json())
   .then((data) => {
     // Código utilizado con los datos obtenidos
     // Crear div con producto por cada libro registrado. Si pulsamos el botón del carrito, añadimos el producto al carrito a través de la función "addProduct"
-    data.forEach((item) => {
+    data.forEach((book, index) => {
       cartContainerTable.innerHTML += `
       <div class="pro">
-                    <img src="images/update/products/${item.isbn}.png" alt="">
+                    <img src="images/update/products/${book.isbn}.png" alt="">
                     <div class="des">
-                        <span>${item.categoryName}</span>
-                        <h3>${item.title}</h3>
-                        <h5>${item.authorName}</h5>
-                        <h4>${item.price} €</h4>
+                        <span>${book.categoryName}</span>
+                        <h3>${book.title}</h3>
+                        <h5>${book.authorName}</h5>
+                        <h4>${book.price} €</h4>
                     </div>
-                    <a onclick="addProduct(${item.isbn})" class="add-to-cart-button"><i class="fal fa-shopping-cart cart"></i></a>
+                    <a onclick="(() => addProduct(${index}))()" class="add-to-cart-button"><i class="fal fa-shopping-cart cart"></i></a>
+
                 </div>
       `;
     });
-    // TODO: Realizar cambios en el número de productos del icono de carrito y añadirlos
+    // console.log(data); // El array "data" es accesible aquí
+    // TODO: Añadir datos de array a variable global
+    return data;
   })
   // Si hay un error, mostrar por consola
   .catch((error) => {
     console.error("Error al obtener los productos:", error);
   });
 
-  // Función que añade el producto al carrito
-function addProduct(isbn) {
-  console.log(isbn);
+fetchPromise.then((res) => {
+  dataArray = Object.values(res);
+});
+
+// Función que añade el producto al carrito
+function addProduct(index) {
+  console.log(dataArray[index]);
 }
