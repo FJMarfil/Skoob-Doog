1171761989aad4f484adf37466705feab2f18542
FJMarfil <Franciski_98@hotmail.com>
2023-04-24 17:59:21 +0200

    Update: 'shop copy' name to 'shop'

--- shop.php
+++ shop.php
@@ -2,16 +2,16 @@
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="author" content="Francisco Jesús Marfil Vargas" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Francisco Jesús Marfil Vargas">
 
     <title>Tienda - Skoob Doog</title>
 
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
 
-    <link rel="stylesheet" href="styles/style.css" />
+    <link rel="stylesheet" href="styles/style.css">
 
     <!--TODO: Quitar enlaces comentados-->
     <!--
@@ -21,17 +21,16 @@
 -->
 
     <?php
+
+    // Iniciar la sesión
+    if (session_status() == 1) {
+        session_start();
+    }
+
     // Conectar a la base de datos
     require("php/connect-to-database.php");
-    // Añadir la función para obtener productos
-    require("php/get-products.php");
-    // Añadir función que obtiene el nombre de autor según un ISBN dado
-    require("php/get-author-name.php");
-    // Añadir función que obtiene la categoría de un libro según un ISBN dado
-    require("php/get-category-name.php");
 
     ?>
-
 </head>
 
 <body>
@@ -48,41 +47,13 @@
     </section>
     <section id="product1" class="section-p1">
         <div class="pro-container">
-
-            <?php
-            // Llamar a la función getProducts para obtener los datos de los productos de la base de datos
-            $products = getProducts();
-
-            // Recorrer el array obteniendo los datos
-            foreach ($products as $value) {
-            ?>
-                <div class="pro" onclick="window.location.href='productox.html';">
-                    <img src="images/update/products/<?php echo $value['isbn']; ?>.png" alt="">
-                    <div class="des">
-                        <span><?php echo getCategoryName($value['category']); ?></span>
-                        <h3><?php echo $value['title']; ?></h3>
-                        <h5><?php echo getAuthorName($value['author']); ?></h5>
-                        <h4><?php echo $value['price']; ?> €</h4>
-                    </div>
-                    <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
-                </div>
-            <?php
-            }
-            ?>
         </div>
     </section>
 
     <?php
-    // Liberar memoria y cerrar la conexión con la base de datos
-    mysqli_free_result($result);
+    // Cerrar la conexión con la base de datos
     mysqli_close($conn);
     ?>
-
-    <section id="pagination" class="section-p1">
-        <a href="#">1</a>
-        <a href="#">2</a>
-        <a href="#"><i class="fal fa-long-arrow-alt-right"></i></a>
-    </section>
 
     <!--Boletín de noticias-->
     <section id="newsletter" class="section-p1 section-m1">
@@ -103,7 +74,25 @@
     <?php include "footer.html" ?>
 
 
-    <script src="script.js"></script>
+    <div id="modalCart">
+        <div class="cart-wrapper">
+            <h2>Carrito</h2>
+            <div class="cart-container">
+            </div>
+            <div class="cart-actions">
+                <button class="clear-cart">Vaciar carrito</button>
+                <button class="buy-cart">Comprar</button>
+                <button class="close-cart">Cerrar</button>
+                <span class="total-price"></span>
+            </div>
+        </div>
+    </div>
+
+    <script src="scripts/script.js"></script>-
+
+    <!--Script del carrito-->
+    <script src="scripts/shop.js"></script>
+
 </body>
 
 </html>
