c7242f57f5d68e16920e1c1a5c4408b83b06284d
FJMarfil <Franciski_98@hotmail.com>
2023-04-27 14:47:39 +0200

    Add: modal cart file

---
+++ php/modal-cart.php
@@ -0,0 +1,13 @@
+<div id="modalCart">
+        <div class="cart-wrapper">
+            <h2>Carrito</h2>
+            <div class="cart-container">
+            </div>
+            <div class="cart-actions">
+                <button class="clear-cart">Vaciar carrito</button>
+                <button class="buy-cart">Comprar</button>
+                <button class="close-cart">Cerrar</button>
+                <span class="total-price"></span>
+            </div>
+        </div>
+    </div>
