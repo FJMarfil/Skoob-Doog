7c6dc3595026c9845de3c939fe56565bc2794e07
FJMarfil <Franciski_98@hotmail.com>
2023-05-24 21:33:52 +0200

    Update: form data

--- purchase.php
+++ purchase.php
@@ -25,56 +25,57 @@
     </section>
 
     <section id="purchase-form-email" class="section-p1 form">
-        <!--Correo electrónico-->
-        <h4>Introduce tu correo electrónico</h4>
+        <form action="/process-purchase-email" method="POST">
+            <!--Correo electrónico-->
+            <h4>Introduce tu correo electrónico</h4>
+            <label for="email">Correo electrónico</label>
+            <input type="email" id="email" name="email" required>
 
-        <label for="email">Correo electrónico</label>
-        <input type="email" id="email" name="email" required>
-
-        <input type="submit" value="Continuar">
+            <input type="submit" id="purchase-form-button" value="Continuar">
+        </form>
     </section>
 
     <!--Formulario de compra-->
     <section id="purchase-form" class="section-p1 form">
-        <form action="/procesar_compra" method="POST">
+        <form action="/process-purchase" method="POST">
             <!--Detalles del cliente-->
             <h4>Introduce tus datos</h4>
             <label for="name">Nombre</label>
-            <input type="text" id="name" name="name" required><br>
+            <input type="text" id="name" name="name" required>
 
             <label for="surname">Apellidos</label>
-            <input type="text" id="surname" name="surname" required><br>
+            <input type="text" id="surname" name="surname" required>
 
             <label for="email">Correo electrónico</label>
-            <input type="email" id="email" name="email" required><br>
+            <input type="email" id="email" name="email" required>
 
             <label for="email">Teléfono</label>
-            <input type="number" id="phone" name="phone" required><br>
+            <input type="number" id="phone" name="phone" required>
 
             <!--Detalles del envío-->
             <h4>Detalles del envío</h4>
             <label for="address">Dirección de envío</label>
-            <input type="text" id="address" name="address" required><br>
+            <input type="text" id="address" name="address" required>
 
             <label for="city">Ciudad</label>
-            <input type="text" id="city" name="city" required><br>
+            <input type="text" id="city" name="city" required>
 
             <label for="postal-code">Código postal</label>
-            <input type="text" id="postal-code" name="postal-code" maxlength="5" pattern="[0-9]{5}" required><br>
+            <input type="text" id="postal-code" name="postal-code" maxlength="5" pattern="[0-9]{5}" required>
 
             <label for="province">Provincia</label>
-            <input type="text" id="province" name="province" required><br>
+            <input type="text" id="province" name="province" required>
 
             <!--Información de pago-->
             <h4>Información de pago</h4>
             <label for="card">Número de tarjeta</label>
-            <input type="text" id="Card" name="Card" required><br>
+            <input type="text" id="Card" name="Card" required>
 
             <label for="expiration-date">Fecha de vencimiento</label>
-            <input type="text" id="expiration-date" name="expiration-date" required><br>
+            <input type="text" id="expiration-date" name="expiration-date" required>
 
             <label for="cvv">CVV</label>
-            <input type="text" id="cvv" name="cvv" required><br>
+            <input type="text" id="cvv" name="cvv" required>
 
             <input type="submit" value="Finalizar Compra">
         </form>
