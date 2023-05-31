13c77d3a42a0e303b19cfb3b1f3c79b6e08e2737
FJMarfil <Franciski_98@hotmail.com>
2023-05-31 20:31:18 +0200

    Add: shipping price span

--- php/modal-cart.php
+++ php/modal-cart.php
@@ -3,6 +3,9 @@
             <h2>Carrito</h2>
             <div class="cart-container">
             </div>
+            <div>
+                <span id="shipping-price">Total incluye costes de envío (4 €)</span>
+            </div>
             <div class="cart-actions">
                 <button class="clear-cart">Vaciar carrito</button>
                 <button class="buy-cart">Comprar</button>
