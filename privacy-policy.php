45823e8a253faaae8dd8ad606646015dc43338d4
FJMarfil <Franciski_98@hotmail.com>
2023-06-02 14:27:06 +0200

    Update: clean code

--- privacy-policy.php
+++ privacy-policy.php
@@ -2,18 +2,18 @@
 <html lang="es">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="author" content="Francisco Jesús Marfil Vargas" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Francisco Jesús Marfil Vargas">
 
   <title>Política de privacidad - Skoob Doog</title>
 
   <!--Importar la librería font-awesome (contiene fuentes e iconos que se utilizarán en la web)-->
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
 
   <!--Importar la hoja de estilos-->
-  <link rel="stylesheet" href="styles/style.css" />
+  <link rel="stylesheet" href="styles/style.css">
 </head>
 
 <body>
